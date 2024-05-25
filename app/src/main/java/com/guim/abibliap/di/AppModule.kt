b69aa66aca45271fbbbf0089ed1bce4b46ccf604
Faguim <fagnermuniz12@gmail.com>
2024-05-25 20:05:51 -0300

    feat: create injection from depenedency

---
+++ app/src/main/java/com/guim/abibliap/di/AppModule.kt
@@ -0,0 +1,52 @@
+package com.guim.abibliap.di
+
+import com.guim.abibliap.data.BibleApi
+import com.guim.abibliap.data.repository.RemoteDataRepositoryImpl
+import com.guim.abibliap.domain.repository.RemoteDataRepository
+import com.guim.abibliap.domain.use_case.FindAllBooksUseCase
+import com.guim.abibliap.domain.use_case.UseCase
+import com.guim.abibliap.util.Constants
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideBibleApi(moshi: Moshi): BibleApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.API_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(BibleApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(api: BibleApi): RemoteDataRepository {
+        return RemoteDataRepositoryImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUseCase(repository: RemoteDataRepository): UseCase {
+        return  UseCase(
+            findAllBooksUseCase = FindAllBooksUseCase(repository)
+        )
+    }
+
+}
