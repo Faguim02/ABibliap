5bfc5c844c022381763af7c5fa426607fca24b8a
Faguim <fagnermuniz12@gmail.com>
2024-05-24 22:27:49 -0300

    dependecy: Configurando as dependencias iniciais: Extended Icon, Navigation, Retrofit, Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -63,4 +65,22 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Extended Icons
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
+
+// Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+
+// Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-compiler:2.48.1")
 }
--- app/src/main/java/com/guim/abibliap/MainActivity.kt
+++ app/src/main/java/com/guim/abibliap/MainActivity.kt
@@ -18,26 +18,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ABibliapTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ABibliapTheme {
-        Greeting("Android")
     }
 }
