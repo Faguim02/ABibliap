a871d6321ceebd62eac828faff340fc1f8a3b9c5
Faguim <fagnermuniz12@gmail.com>
2024-05-27 15:07:45 -0300

    feat: implements in MainViewModel is methodos findChapter and findOneBook

--- app/src/main/java/com/guim/abibliap/presentation/MainViewModel.kt
+++ app/src/main/java/com/guim/abibliap/presentation/MainViewModel.kt
@@ -2,7 +2,12 @@ package com.guim.abibliap.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.guim.abibliap.data.dto.book.toBookData
 import com.guim.abibliap.data.dto.books.toBooksData
+import com.guim.abibliap.data.dto.chapter.Book
+import com.guim.abibliap.data.dto.chapter.Chapter
+import com.guim.abibliap.data.dto.chapter.Verse
+import com.guim.abibliap.data.dto.chapter.toChapterData
 import com.guim.abibliap.domain.model.book.BookData
 import com.guim.abibliap.domain.model.books.BooksData
 import com.guim.abibliap.domain.model.chapter.ChapterData
@@ -24,6 +29,12 @@ class MainViewModel @Inject constructor(
     private val _booksDataResponse = MutableStateFlow<Result<BooksData>>(Result.Idle)
     val booksDataResponse = _booksDataResponse.asStateFlow()
 
+    private val _bookDataResponse = MutableStateFlow<Result<BookData>>(Result.Idle)
+    val bookDataResponse = _bookDataResponse.asStateFlow()
+
+    private val _chapterDataResponse = MutableStateFlow<Result<ChapterData>>(Result.Idle)
+    val chapterDataResponse = _chapterDataResponse.asStateFlow()
+
     fun findAllBooks() = viewModelScope.launch {
         useCase.findAllBooksUseCase.invoke()
             .onStart {
@@ -34,6 +45,39 @@ class MainViewModel @Inject constructor(
                 val result = it.toBooksData()
                 _booksDataResponse.value = Result.Success(result)
             }
+    }
+
+    fun findOneBook(abbrev: String) = viewModelScope.launch {
+        useCase.findOneBookUseCase.invoke(abbrev = abbrev)
+            .onStart {
+                _bookDataResponse.value = Result.Loading
+            }.catch {
+                _bookDataResponse.value = Result.Error(it)
+            }.collect {
+                val result = it.toBookData()
+                _bookDataResponse.value = Result.Success(result)
+            }
+    }
+
+    fun findChapter(
+        abbrev: String,
+        chapter: String,
+        version: String
+    ) = viewModelScope.launch {
+
+        useCase.findChapterUseCase.invoke(
+            chapter = chapter,
+            version = version,
+            abbrev = abbrev
+        ).onStart {
+            _chapterDataResponse.value = Result.Loading
+        }.catch {
+            _chapterDataResponse.value = Result.Error(it)
+        }.collect {
+            val result = it.toChapterData()
+            _chapterDataResponse.value = Result.Success(result)
+        }
+
     }
 
 }
