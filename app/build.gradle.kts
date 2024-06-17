a01379ba1d0de04becfb01ba345ad2321f7f3934
Isaac Martins <isaac.martins@redes.unb.br>
2024-06-17 09:51:08 -0300

    build/atualização no build.gradle app adicionando as dependencias do ROOM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,10 +69,15 @@ dependencies {
     // Extended Icons
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
 
-// Navigation
+    // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.6")
 
-// Retrofit
+    // Room
+    implementation ("androidx.room:room-runtime:2.6.1")
+    kapt ("androidx.room:room-compiler:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
