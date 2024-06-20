10905f5d4c1e6107f3bf3aefb4ccbe4b4cf46e92
Faguim <fagnermuniz12@gmail.com>
2024-06-20 20:42:51 -0300

    refactor: trocando dados fakes pelos da api em /HomeScreen.kt

--- app/src/main/java/com/guim/abibliap/presentation/home/home_screen/HomeScreen.kt
+++ app/src/main/java/com/guim/abibliap/presentation/home/home_screen/HomeScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -14,70 +16,20 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import com.guim.abibliap.data.dto.book.Abbrev
 import com.guim.abibliap.domain.model.book.BookData
+import com.guim.abibliap.domain.model.books.BooksData
 import com.guim.abibliap.presentation.MainViewModel
 import com.guim.abibliap.presentation.home.home_components.BookComponent
 import com.guim.abibliap.ui.theme.Merriweather
+import com.guim.abibliap.util.Result
 
 @Composable()
-fun HomeScreen(
-    navController: NavController,
-    mainViewModel: MainViewModel = hiltViewModel(),
-) {
+fun HomeScreen(navController: NavController, viewModel: MainViewModel = hiltViewModel()) {
 
-    val booksData = listOf<BookData>(
-        BookData(
-            name = "Gênesis",
-            group = "Pentateuco",
-            author = "Moisés",
-            testament = "Antigo Testamento",
-            abbrev = Abbrev(en = "gn", pt = "gn"),
-            chapters = 50
-        ),
-        BookData(
-            name = "Êxodo",
-            group = "Pentateuco",
-            author = "Moisés",
-            testament = "Antigo Testamento",
-            abbrev = Abbrev(en = "ex", pt = "ex"),
-            chapters = 40
-        ),
-        BookData(
-            name = "Levítico",
-            group = "Pentateuco",
-            author = "Moisés",
-            testament = "Antigo Testamento",
-            abbrev = Abbrev(en = "lv", pt = "lv"),
-            chapters = 27
-        ),
-        BookData(
-            name = "Números",
-            group = "Pentateuco",
-            author = "Moisés",
-            testament = "Antigo Testamento",
-            abbrev = Abbrev(en = "nm", pt = "nm"),
-            chapters = 36
-        ),
-        BookData(
-            name = "Deuteronômio",
-            group = "Pentateuco",
-            author = "Moisés",
-            testament = "Antigo Testamento",
-            abbrev = Abbrev(en = "dt", pt = "dt"),
-            chapters = 34
-        ),
-        // ... omitidos para abreviar o exemplo
-        BookData(
-            name = "Apocalipse",
-            group = "",
-            author = "João",
-            testament = "Novo Testamento",
-            abbrev = Abbrev(en = "rv", pt = "ap"),
-            chapters = 22
-        )
-    )
+    val booksResponse = viewModel.booksDataResponse.collectAsState().value
 
     Scaffold(
         topBar = {
@@ -102,25 +54,43 @@ fun HomeScreen(
             modifier = Modifier.padding(padding)
         ) {
 
-            LazyColumn {
-                item {
-                    IconButton(
-                        onClick = {}
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.PlayArrow,
-                            contentDescription = null
-                        )
-                    }
+            when(booksResponse) {
+                is Result.Loading -> {
+                    Text(("Carregando"))
                 }
-
-                items(booksData) {
-                    BookComponent(bookData = it)
+                is Result.Error -> {
+                    Text("Erro")
                 }
+                is Result.Success -> {
+                    val booksResult = booksResponse.data
+                    ListForBooks(booksResult)
+                }
+                else -> Unit
             }
+
         }
     }
 
+}
+
+@Composable
+fun ListForBooks(booksData: List<BookData>) {
+    LazyColumn {
+        item {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.PlayArrow,
+                    contentDescription = null
+                )
+            }
+        }
+
+        items(booksData) {
+            BookComponent(bookData = it)
+        }
+    }
 }
 
 //@Preview()
