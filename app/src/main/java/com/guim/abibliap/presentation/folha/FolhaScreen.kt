232377ec5492d8c958cd0e001d77caed9dc39fff
Faguim <fagnermuniz12@gmail.com>
2024-06-22 20:06:15 -0300

    refactor: alterando importação do modier

--- app/src/main/java/com/guim/abibliap/presentation/folha/FolhaScreen.kt
+++ app/src/main/java/com/guim/abibliap/presentation/folha/FolhaScreen.kt
@@ -8,12 +8,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.guim.abibliap.ui.theme.ABibliapTheme
-import java.lang.reflect.Modifier
 
 @Composable
 fun FolhaScreen() {
